82851cb4237fbaa2faefec5e22c2f3332043a794
Gabriel Vaz <gabrielcvaz@outlook.com>
2018-05-15 20:49:35 -0300

    promises

--- app2/src/app/home/home.component.ts
+++ app2/src/app/home/home.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+import { Oferta } from "./../shared/oferta.model";
+import { OfertasService } from "./../ofertas.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
+  public ofertas: Oferta[];
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) {}
 
   ngOnInit() {
-  }
+    // this.ofertas = this.ofertasService.getOfertas();
 
+    this.ofertasService.getOfertas2()
+      .then((ofertas: Oferta[]) => {
+        this.ofertas = ofertas;
+      });
+  }
 }
